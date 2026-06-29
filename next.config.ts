71de7dab1980d03f855ba9eb0ba8333a7e1afeb9
Chad Wright <justdeejays@gmail.com>
2026-06-29 20:01:51 +0200

    fix: add firebase-admin to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ["firebase-admin"],
   images: {
     deviceSizes: [425, 800, 1280],
     minimumCacheTTL: 31536000,
